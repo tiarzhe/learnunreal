95797ac47ccfecc3339d134ce6a8d6ea72677581
titarzhe <2809376822@qq.com>
2018-05-16 15:52:48 +0800

    c文件上传

---
+++ Source/learnunreal/learnunreal.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "learnunreal.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, learnunreal, "learnunreal" );
---
+++ Source/learnunreal/learnunrealGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "learnunrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNUNREAL_API AlearnunrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
